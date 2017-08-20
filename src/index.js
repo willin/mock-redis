468eb14c8e807441bf22040d20b2602c43ecf1ae
Willin Wang <willin@willin.org>
2017-08-20 09:45:47 +0800

    unstaging version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-module.exports = '';
+const data = {};
+
+module.exports = {
+  get(key) {
+    if (data[key] && new Date().getTime() - data[key].expires > 0) {
+      return data[key].val;
+    }
+    return null;
+  },
+  setex(key, expires, val) {
+    data[key] = {
+      val,
+      expires: new Date().getTime() + expires * 1000
+    };
+    return true;
+  },
+  set(key, val) {
+    return this.setex(key, 0, val);
+  }
+};
